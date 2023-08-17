89fdb8d31b7a3e1536f10a2025f64fda8596f691
ShimaSh1ma <D1528786994@gmail.com>
2023-08-17 22:09:03 +0800

    init

---
+++ PixivDownloadHelper/DownloadMode/HttpDownload.cpp
@@ -0,0 +1,89 @@
+﻿#include "Crawler.h"
+
+int MHttpDownload::parseHtmlForUrl(const std::string& html, std::vector<std::string>& U, const std::string& rule)
+{
+		std::regex geturl_pattern(rule);
+		int count = 0;
+
+		const std::sregex_token_iterator end;
+		for (std::sregex_token_iterator iter(html.begin(), html.end(), geturl_pattern); iter != end; ++iter)
+		{
+			U.push_back(iter->str());
+			++count;
+		}
+		return count;
+}
+
+
+std::string MHttpDownload::requestHtml(const UrlParser& URL, const std::string& request)
+{
+	std::string http = _EMPTY_STRING;			//接收报文字符串
+
+	std::string _Host = URL.host;
+	std::string _Source = URL.source;
+	std::string _Protocol = URL.protocol;
+	std::string _Port;
+	if (_Protocol == "https") {
+		_Port = "443";
+	}
+	else if(_Protocol == "http"){
+		_Port = "80";
+	}
+	else {
+		return _EMPTY_STRING;
+	}
+
+	if (socketInit(_Host,_Port)) {
+		if (sslInit()) {
+			if (sockConnect()) {
+				if (sslConnect(_Host)) {
+					std::string* sendbuffer = new std::string(request);
+					if (socketSend(*sendbuffer)) {
+						delete sendbuffer;
+						http = socketReceiveHtml();
+						sslClose();
+						socketClose();
+					}
+				}
+			}
+		}
+	}
+	return http;
+}
+
+bool MHttpDownload::fileDownload_nonreuse(const UrlParser& URL, const std::string& download_dir, const std::string& request)
+{
+	std::string _Host = URL.host;
+	std::string _Source = URL.source;
+	std::string _Protocol = URL.protocol;
+	std::string _Port;
+	if (_Protocol == "https") {
+		_Port = "443";
+	}
+	else if (_Protocol == "http") {
+		_Port = "80";
+	}
+	else {
+		return false;
+	}
+
+	if (socketInit(_Host, _Port)) {
+		if (sslInit()) {
+			if (sockConnect()) {
+				if (sslConnect(_Host)) {
+					std::string* sendbuffer = new std::string(request);
+					if (socketSend(*sendbuffer)) {
+						delete sendbuffer;
+						socketReceiveFile(download_dir);
+						sslClose();
+						socketClose();
+					}
+				}
+			}
+		}
+	}
+	if (returnLog() != _DOWNLOAD_SUCCESS){
+		return false;
+	}
+	return true;
+}
---
+++ PixivDownloadHelper/DownloadMode/HttpRequest.cpp
@@ -0,0 +1,58 @@
+﻿#include "basicClass.h"
+
+HttpRequest::HttpRequest() :urlSource({}), urlHost({}) {}
+
+HttpRequest::HttpRequest(UrlParser& url) : urlSource(url.source), urlHost(url.host) {}
+
+void HttpRequest::refreshUrl(UrlParser& url)
+{
+	this->urlHost = url.host;
+	this->urlSource = url.source;
+}
+
+std::string HttpRequest::request() {
+	std::string  _url_host;
+	std::string _accept;
+	std::string _accept_charset;
+	std::string _accept_language;
+	std::string _referer;
+	std::string _cookie;
+	std::string _user_agent;
+	std::string _connection;
+
+	if (urlHost != "") {
+		_url_host = "Host:" + urlHost + "\r\n";
+	}
+	if (accept != "") {
+		_accept = "Accept:" + accept + "\r\n";
+	}
+	if (acceptCharset != "") {
+		_accept_charset = "Accept-Charset:" + acceptCharset + "\r\n";
+	}
+	if (acceptLanguage != "") {
+		_accept_language = "Accept-Language:" + acceptLanguage + "\r\n";
+	}
+	if (referer != "") {
+		_referer = "Referer:" + referer + "\r\n";
+	}
+	if (cookie != "") {
+		_cookie = "Cookie:" + cookie + "\r\n";
+	}
+	if (userAgent != "") {
+		_user_agent = "User-Agent:" + userAgent + "\r\n";
+	}
+	if (connection != "") {
+		_connection = "Connection:" + connection + "\r\n";
+	}
+
+	return method + " " + urlSource + " " + httpVersion + "\r\n" 
+		+ _url_host
+		+ _accept 
+		+ _accept_charset
+		+ _accept_language
+		+ _referer
+		+ _cookie
+		+ _user_agent
+		+ _connection
+		+ "\r\n";
+}
